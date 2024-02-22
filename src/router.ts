eefab51768f254872dd3047127119d7fa9a8ecf4
jp-atlantis <jpaiao@atlantis-ssz.com.br>
2024-02-22 18:07:21 -0300

    feat: Adicionado as rotas de usuarios

--- src/router.ts
+++ src/router.ts
@@ -6,9 +6,11 @@ const app = express()
 app.use(express.json())
 
 app.get('/', getAllCalls)
+app.post('/call/set', setCall)
+app.put('/call/updateStatus', routerUpdate)
 
-app.post('/setCall', setCall)
-
-app.put('/updateStatus', routerUpdate)
+app.post('/user/User', )
+app.put('/user/update', )
+app.delete('/user/delete', )
 
 export { app }

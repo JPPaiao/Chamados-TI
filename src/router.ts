8e1ae1e42f6e60074368bc3557959bc2e3a4a63a
jplp <jp.paiao03@gmail.com>
2024-02-22 00:06:36 -0300

    feat: Rota de criar chamados adicionada

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,9 @@ import { callList } from './models/listCall'
 const router = express.Router()
 
 router.get('/', (req, res) => res.send(callList))
-// router.post('/setCall', (req, res) => )
+router.post('/setCall', (req, res) => {
+    const called = req.body
+    console.log(called)
+})
 
 export default router

bfab1a7d5c216ec7d9dc1e55cbb92cab59cf4405
jplp <jp.paiao03@gmail.com>
2024-02-24 19:04:03 -0300

    chore: Add novas libs

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -3,16 +3,22 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  "parser": "@typescript-eslint/parser",
+  "plugins": [
+    "@typescript-eslint",
+    "react-refresh"
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'no-unused-vars': 'off',
+    "react-refresh/only-export-components": "error",
+    '@typescript-eslint/no-unused-vars': 'off',
+    "@typescript-eslint/no-explicit-any": ["off"]
   },
 }

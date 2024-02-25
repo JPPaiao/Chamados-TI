ca49eae1223bff4b876604b047ad63eeae71e029
jplp <jp.paiao03@gmail.com>
2024-02-25 16:06:07 -0300

    style: Add o header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,15 +1,16 @@
 import Logo from "../assets/logo.png"
+import { UserIcon } from "./icons/user"
 
-export interface IAppProps {
-}
-
-function Header (props: IAppProps) {
+function Header() {
   return (
-    <div className="w-full">
-			<div className=" md:max-w-[960px] m-auto bg-red-300">
+    <div className="w-full shadow-lg z-50">
+			<div className="px-10 py-2 flex justify-between items-center">
 				<div className="">
 					<img className="w-28" src={Logo} alt="Logo Atlantis" />
 				</div>
+				<button className="rounded cursor-pointer hover:bg-green-200 p-2 transition-all duration-200">
+					<UserIcon width="w-10" />
+				</button>
 			</div>
     </div>
   )

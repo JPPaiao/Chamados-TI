bc7181b2f4465cc1167cf4a09bcc35d24d631250
jp-atlantis <jpaiao@atlantis-ssz.com.br>
2024-02-26 18:00:04 -0300

    feat: Adicionado rotas com o react router dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,8 @@
-import { Dashboard } from "./page/dashboard"
-import { MagicMotion } from "react-magic-motion";
-
 function App() {
   return (
-    <MagicMotion>
-      <div className='text-2xl text-black flex flex-col w-screen h-screen'>
-        <Dashboard />
-      </div>
-    </MagicMotion>
+    <div className='text-2xl text-black flex flex-col h-screen'>
+      
+    </div>
   )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import { Dashboard } from './page/dashboard.tsx'
+import { Process } from './components/process.tsx'
+import { ListCalled } from './components/listCalleds.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    children: [
+      {
+        path: "processos",
+        element: <Process />
+      },
+      {
+        path: "chamados",
+        element: <ListCalled />
+      },
+    ]
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />  
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

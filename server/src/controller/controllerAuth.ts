fa69db89d9f9949e49dc8e34208a4ce29fda543d
jp-atlantis <jpaiao@atlantis-ssz.com.br>
2024-03-02 14:13:16 -0300

    feat: Adicionado função de autenticação

--- server/src/controller/controllerAuth.ts
+++ server/src/controller/controllerAuth.ts
@@ -1,5 +1,25 @@
-import { sign } from 'jsonwebtoken'
+import { verify } from 'jsonwebtoken'
+import { NextFunction, Request, Response } from "express"
 
 const secretKey = 'skljaksdj9983498327453lsldkj@@#'
 
+const verifyAuth = (req: Request, res: Response, next: NextFunction) => {
+  const tokenHeader = req.headers["auth"] as string
+  const token = tokenHeader.split(" ")[1]
 
+  if (!tokenHeader) {
+    res.status(401).json({ menssage: 'Erro: Usuário sem autorização para esta ação' })
+  } 
+  console.log(tokenHeader)
+  
+  try {
+    console.log(token)
+    
+    verify(token, secretKey)
+    next()
+  } catch (err) {
+    res.status(500).json({ menssage: 'Erro: Token não valido' })
+  }
+}
+
+export { verifyAuth }

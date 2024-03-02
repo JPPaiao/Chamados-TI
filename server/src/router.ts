d8401e427d2e8c2c8b24387da0ad2c9c421b5430
jp-atlantis <jpaiao@atlantis-ssz.com.br>
2024-03-02 11:38:46 -0300

    feat: Adicionado nova rota de login

--- server/src/router.ts
+++ server/src/router.ts
@@ -2,19 +2,19 @@ import cors from "cors"
 import express from "express"
 import { Request, Response } from "express"
 import { getAllCalls, setCall, routerUpdate } from "./controller/controllerCall"
-import { deleteUser, getUsers, setUser, updateUser } from "./controller/controllerUsers"
+import { deleteUser, getUsers, setUser, updateUser, login } from "./controller/controllerUsers"
 
 const app = express()
 
 app.use((req: Request, res: Response, next) => {
-    cors()
-    next()
+	cors()
+	next()
 })
 app.use((req: Request, res: Response, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Methods", 'GET, PUT, POST, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-    next()
+	res.setHeader("Access-Control-Allow-Origin", "*")
+	res.setHeader("Access-Control-Allow-Methods", 'GET, PUT, POST, DELETE')
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+	next()
 })
 
 app.use(express.json())
@@ -23,9 +23,13 @@ app.get('/api/', getAllCalls)
 app.post('/api/call/set', setCall)
 app.put('/api/call/updateStatus', routerUpdate)
 
+app.post('/auth/login', login)
+
 app.post('/api/user/users', getUsers)
 app.post('/api/user/set', setUser)
 app.put('/api/user/update', updateUser)
 app.delete('/api/user/delete', deleteUser)
+
+
 
 export { app }

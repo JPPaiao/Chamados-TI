1a4ef0c1fa0eb5dae39ffcaf758864c52ccdeab7
jp-atlantis <jpaiao@atlantis-ssz.com.br>
2024-03-05 18:05:11 -0300

    refactor: Refatorado rotas de usuários

--- server/src/router.ts
+++ server/src/router.ts
@@ -4,7 +4,9 @@ import { Request, Response } from "express"
 import { getAllCalls, setCall, routerUpdate } from "./controller/controllerCall"
 import { deleteUser, getUsers, setUser, updateUser, login } from "./controller/controllerUsers"
 import { verifyAuth } from "./controller/controllerAuth"
-import { CreateCustomerController } from "./controller/controllerCreateCustomer"
+import { CreateUserController } from "./controller/users/createUser"
+import { ListUsersController } from "./controller/users/listUsers"
+import { UpdateUserController } from "./controller/users/updateUser"
 
 const app = express()
 
@@ -22,7 +24,7 @@ app.use((req: Request, res: Response, next) => {
 app.use(express.json())
 
 app.get('/test', async (req: Request, res: Response) => {
-	return new CreateCustomerController().handle(req, res)
+	return new CreateUserController().handle(req, res)
 })
 
 app.get('/api/', verifyAuth, getAllCalls)
@@ -31,9 +33,17 @@ app.put('/api/call/updateStatus', verifyAuth, routerUpdate)
 
 app.post('/auth/login', login)
 
-app.post('/api/user/users', verifyAuth, getUsers)
-app.post('/api/user/set', setUser)
-app.put('/api/user/update', updateUser)
+app.post('/api/user/users', verifyAuth, async (req: Request, res: Response) => {
+	return new ListUsersController().handle(req, res)
+})
+
+app.post('/api/user/create', async (req: Request, res: Response) => {
+	return new CreateUserController().handle(req, res)
+})
+
+app.put('/api/user/update',  async (req: Request, res: Response) => {
+	return new UpdateUserController().handle(req, res)
+})
 app.delete('/api/user/delete', deleteUser)
 
 export { app }

cb30783bd1cae30aefb9fe1f20e5cfb2e0b4f00e
jp-atlantis <jpaiao@atlantis-ssz.com.br>
2024-03-26 17:19:51 -0300

    feat: Adiciondor função de adicionar PDF ao processo

--- client/src/components/process.tsx
+++ client/src/components/process.tsx
@@ -15,10 +15,20 @@ interface Process {
 	URL: string 
 }
 
+interface Response {
+	id: string | undefined,
+	message: string | undefined
+}
+
 function Process() {
 	const [process, setProcess] = useState<Process[]>([])
 	const [openCards, setOpenCards] = useState<boolean[]>(Array(process.length).fill(false))
 	const [newProcess, setNewProcess] = useState<string>()
+	const [newPDF, setNewPDF] = useState<File | null>(null)
+	const [responseMessage, setResponseMessage] = useState<Response>({
+		id: undefined,
+		message: undefined
+	})
 
 	useEffect(() => {
 		fetch('http://localhost:3000/api/procedures', {
@@ -38,12 +48,32 @@ function Process() {
 
 			setProcess(data)
 		})
-	}, [])
+	}, [responseMessage])
 
+	const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if (e.target.files && e.target.files.length > 0) {
+			setNewPDF(e.target.files[0])
+		}
+	}
 	
-	const toggleAddProcess = (e: any) => {
+	const toggleAddProcess = async (e: any) => {
 		e.preventDefault()
 		
+		const formDataToSend: any = new FormData()
+		formDataToSend.append('title', newProcess)
+		formDataToSend.append('description', 'descrição')
+		formDataToSend.append('author', 'autor')
+		formDataToSend.append('sector', 'setor')
+		formDataToSend.append('pdfName', newPDF?.name)
+		formDataToSend.append('pdf', newPDF as File)
+		
+		const formResponse = await fetch('http://localhost:3000/api/procedures/create', {
+			method: 'post',
+			body: formDataToSend
+		})
+		.then(d => d.json())
+		
+		setResponseMessage(formResponse as Response)
 	}
 
   const toggleCard = (index: number) => {
@@ -53,15 +83,21 @@ function Process() {
   }
 
 	return (
-		<div className="flex gap-4 px-6 py-4 flex-wrap items-center justify-center">
-			<form className="flex justify-around w-full text-base" onSubmit={(e) => toggleAddProcess(e)}>
+		<div className="flex gap-4 px-6 py-4 flex-wrap items-center justify-center flex-co">
+			{/* {responseMessage.message && ( 
+				<div className="absolute text-xl bg-green-400 border-2 border-black text-black shadow px-3 py-1">
+					{responseMessage.message}
+				</div>
+			)} */}
+			<form className="flex justify-around w-full text-sm" onSubmit={(e) => toggleAddProcess(e)}>
 				<input onChange={(e) => setNewProcess(e.target.value)} type="text" placeholder="Título" className="px-2 outline-none border-b-2 border-green-800" />
+				<input onChange={(e) => handleFileChange(e)} type="file" placeholder="PDF" className="pr-2 outline-none border-b-2 border-green-800 cursor-pointer" />
 				<input type="submit" value="Adicionar processo" className="px-3 cursor-pointer hover:bg-green-800 hover:text-white transition-all shadow-md border-green-800 border-2 bg-green-50" />
 			</form>
 			<div className="flex gap-2 py-3 flex-wrap items-center justify-center w-full">
 				{
 					process.map((process: Process, index: number) => (
-						<div key={process.procedure.title} className={`px-3 max-w-96 w-full flex flex-col items-center justify-between shadow-md border-green-800 border-2 bg-green-50 ${
+						<div key={process.procedure.id} className={`px-3 max-w-96 w-full flex flex-col items-center justify-between shadow-md border-green-800 border-2 bg-green-50 ${
 								!openCards[index] ? 'h-auto' : 'h-auto'
 							}`}>
 								<div className="w-full flex items-center justify-between">
@@ -79,7 +115,7 @@ function Process() {
 								<div className="text-base py-1">
 									{process.procedure.description}
 									<div>
-										<a href={process.URL} download={`${process.name}`} >{process.name}</a>
+										<a href={process.URL} target="_blank" >{process.name}</a>
 									</div>
 								</div>
 							)}

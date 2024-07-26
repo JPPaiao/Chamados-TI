f6c749c9b65b95692788ffe522cef6183947b964
jp-atlantis <jpaiao@atlantis-ssz.com.br>
2024-07-26 11:08:35 -0300

    feat: Dados do usuário sendo slavo no localStorage

--- client/src/store/users/userSlice.ts
+++ client/src/store/users/userSlice.ts
@@ -1,29 +1,51 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+
+interface UserTypes {
+  token: string,
+  email: string,
+  username: string,
+  id: string,
+  // role: string
+}
+
+interface InitialStateType {
+  user: UserTypes | null
+}
+
+const getUserLocalStorage = (): UserTypes => {
+  const user = localStorage.getItem('userToken')
+  return user ? JSON.parse(user) : null
+}
+
+const initialState: InitialStateType = {
+  user: getUserLocalStorage()
+}
 
 const userSlice = createSlice({
   name: 'user',
-  initialState: {
-    token: '',
-    email: '',
-    username: '',
-    id: ''
-  },
+  initialState,
   reducers: {
-    userAuth: (state, action) => {
-      state.token = action.payload.token
-      state.email = action.payload.email
-      state.username = action.payload.username
-      state.id = action.payload.id
+    userAuth: (state, action: PayloadAction<UserTypes>) => {
+      state.user = action.payload
+
+      localStorage.setItem('userToken', JSON.stringify(action.payload))
     },
 
     removeAuth: state => {
-      state.token = ''
-      state.email = ''
-      state.username = ''
-      state.id = ''
+      state.user = null
+
+      localStorage.removeItem('userToken')
+    },
+
+    userLogged: () => {
+      const userStorage = getUserLocalStorage()
+
+      if (!userStorage) {
+        initialState.user = userStorage
+      }
     }
   }
 })
 
-export const { removeAuth, userAuth } = userSlice.actions
+export const { removeAuth, userAuth, userLogged } = userSlice.actions
 export default userSlice.reducer

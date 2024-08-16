64e6dc56797a2063697c12473f4406000e094b75
jp-atlantis <jpaiao@atlantis-ssz.com.br>
2024-08-16 08:44:12 -0300

    refactor: Adicionado verifição de login de usuário

--- client/src/components/process.tsx
+++ client/src/components/process.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
 import { PlusCircleIcon } from "./icons/plusCircle"
 import { store } from "../store/store"
-import { Link, useLoaderData } from "react-router-dom"
+import { Link, redirect, useLoaderData } from "react-router-dom"
 import { TrashIcon } from "./icons/trash"
 import { Modal } from "./modal"
 import { Can } from "../middleware/can"
-import { UserTypes } from "../store/users/userSlice"
+import { userLogged, UserTypes } from "../store/users/userSlice"
+const url = import.meta.env.SERVER_API
 
 interface ProcessTypes {
 	id: string,
@@ -24,14 +25,19 @@ interface LoaderTypes {
 }
 
 async function loader() {
-	const user = store.getState().users.user
-	const response = await fetch('http://localhost:3000/api/procedures', {
+	userLogged()
+	const user = store.getState().users
+	const response = await fetch(`http://localhost:3000/api/procedures`, {
 		method: "get",
 		headers: {
 			"Content-Type": "application/json, multipart/form-data, application/pdf",
-			"authorization": user?.token as string
+			"authorization": user?.user?.token as string
 		},
 	}).then(data => data.json())
+
+	if (!user.auth) {
+		return redirect('/')
+	}
 
 	return {
 		process: response ? response : null,
@@ -100,7 +106,7 @@ function Process() {
 	}
 
 	return (
-		<div className="flex gap-4 px-6 py-4 flex-wrap items-center justify-center flex-co">
+		<div className="flex gap-4 px-6 py-4 flex-wrap items-center justify-center flex-col">
 			<Modal mensagen={modalMenssage} actionType={modalAction} isOpen={modalIsOpen} onCancel={closeModal} onConfirm={toggleDeleteProcess} />
 			<div className="flex justify-end">
 				<Can I={['admin']}>
@@ -112,7 +118,7 @@ function Process() {
 			<div className="flex gap-2 flex-wrap items-center justify-center w-full">
 				{
 					process?.map((process: ProcessTypes, index: number) => (
-						<div key={process.id} className={`px-3 max-w-96 w-full flex flex-col items-center justify-between shadow-md border-green-800 border-2 bg-green-50 ${
+						<div key={process.id} className={`px-3 max-w-[340px] w-full flex flex-col items-center justify-between shadow-md border-green-800 border-2 bg-green-50 ${
 								!openCards[index] ? 'h-auto' : 'h-auto'
 							}`}>
 								<div className="w-full flex items-center justify-between">
@@ -125,14 +131,14 @@ function Process() {
 												className="cursor-pointer"
 												onClick={() => handleOpenDeleteModal(process)}
 												>
-												<TrashIcon width="w-5" />
+												<TrashIcon className="w-5" />
 											</div>
 										</Can>
 										<div 
 											className="cursor-pointer"
 											onClick={() => toggleCard(index)}	
 											>
-											<PlusCircleIcon width="w-6" />
+											<PlusCircleIcon className="w-6" />
 										</div>
 									</div>
 								</div>
